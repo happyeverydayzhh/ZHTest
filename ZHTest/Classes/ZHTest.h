f33daaa86b9aafe08e51969d17f0f25f7beddcf9
happyeverydayzhh <happyeverydayzhh@126.com>
2020-09-23 19:56:20 +0800

    project init

---
+++ ZHTest/Classes/ZHTest.h
@@ -0,0 +1,16 @@
+//
+//  ZHTest.h
+//  FBSnapshotTestCase
+//
+//  Created by xiaoZuDeMeng on 2020/9/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZHTest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZHTest/Classes/ZHTest.m
@@ -0,0 +1,12 @@
+//
+//  ZHTest.m
+//  FBSnapshotTestCase
+//
+//  Created by xiaoZuDeMeng on 2020/9/23.
+//
+
+#import "ZHTest.h"
+
+@implementation ZHTest
+
+@end
